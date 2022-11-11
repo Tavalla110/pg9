bba0d37985404b2ac10ba7d8d11039e50cc91e8f
mohammad <pcadocument@gmail.com>
2022-11-11 14:07:34 +0330

    Add project files.

---
+++ pg9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pg9
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter a number:");
+            string str = Console.ReadLine();
+            float val = (float)Convert.ToDouble(str);
+            Console.WriteLine(calculate(val));
+        }
+        private static double calculate(double x)
+        { 
+            double y = Math.Sqrt(x);
+            return y;
+        }
+    }
+}
